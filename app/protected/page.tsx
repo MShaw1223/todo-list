9cd165ea2630d15a999e06cd38c1ae5d59656c44
miller <millershaw61@gmail.com>
2024-04-15 22:05:00 +0100

    changes to auth redirects

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -17,15 +17,6 @@ export default async function ProtectedPage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="w-full">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <AuthButton />
-            <ModeToggle />
-          </div>
-        </nav>
-      </div>
-
       <div className="animate-in flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6">
           <TodoListPage />
--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,5 +1,18 @@
-export default function WelcomeMessage() {
-  return (
+import ProtectedPage from "@/app/protected/page";
+import { createClient } from "@/utils/supabase/server";
+
+export default async function WelcomeMessage() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return user ? (
+    <div className="flex flex-col gap-6 text-center">
+      <ProtectedPage />
+    </div>
+  ) : (
     <div className="flex flex-col gap-6 text-center">
       <p className="mx-4 mr-2 font-bold text-4xl mb-4">Welcome !</p>
       <p className="mx-4 mr-2 font-bold text-4xl mb-4">
