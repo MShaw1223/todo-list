4dd6f69afaa9496596fe82ef8af9d50788248849
miller <millershaw61@gmail.com>
2024-05-31 18:25:17 +0100

    Fix: id property missing
    Add id to tabletype

--- components/DataTable/columns.tsx
+++ components/DataTable/columns.tsx
@@ -1,20 +1,27 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { SetNewCompleteState } from "@/app/protected/actions";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
 export type tableType = {
-  todoid: number;
-  status: "Pending" | "Complete";
+  id: number;
+  complete: boolean;
   body: string;
   header: string;
 };
 
 export const columns: ColumnDef<tableType>[] = [
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "complete",
+    header: "Complete",
   },
   {
     accessorKey: "header",
@@ -23,5 +30,32 @@ export const columns: ColumnDef<tableType>[] = [
   {
     accessorKey: "body",
     header: "Body",
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+      const r = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onClick={() => {
+                SetNewCompleteState(!r.complete, r);
+              }}
+            >
+              Mark as
+              {row.original.complete === true ? " not Complete" : " Complete"}
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
