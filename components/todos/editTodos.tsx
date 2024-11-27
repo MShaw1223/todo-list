6c65eccabbc920f31808140cb272207fdca74da0
miller <millershaw61@gmail.com>
2024-11-27 17:08:03 +0000

    Feat: Edit todos

--- components/todos/editTodos.tsx
+++ components/todos/editTodos.tsx
@@ -1,9 +1,26 @@
-export const EditTodo = () => {
+import { TodoType } from "../DataTable/columns";
+import { ComboBox } from "../ui/combobox";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
+
+export const EditTodo = ({ todos }: { todos: TodoType[] }) => {
+  // filter todos to incomplete
+  // add this to a search select, search done by header name / body contents ; only display header / disp head & small preview text?
   return (
     <>
       <h1 className="mb-2">Edit a todo</h1>
-      <div>
-        <h1>Hello world</h1>
+      <div className="text-center flex flex-wrap min-w-[250px]">
+        // need to access the selectedEdit????
+        <ComboBox todos={todos} />
+        <form>
+          <Input type="text" placeholder="foobar" />
+          <Textarea
+            name="body"
+            id="body"
+            placeholder="Bazbang"
+            className="resize-none"
+          />
+        </form>
       </div>
     </>
   );
